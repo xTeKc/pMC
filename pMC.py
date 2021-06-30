2b885ab07e3fc5ce9cecac3d4cafb91d3fed9e2e
xTeKc <bcd3v@pm.me>
2021-06-29 23:47:32 -0400

    enp0 up

--- pMC.py
+++ pMC.py
@@ -2,3 +2,4 @@ import subprocess
 
 subprocess.call("ifconfig enp0 down", shell=True)
 subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig enp0 up", shell=True)
