24fec9430a2f3d666c60df4caeeb930e7770bdd8
xTeKc <bcd3v@pm.me>
2021-06-29 23:52:07 -0400

    add enp0 to var interface

--- pMC.py
+++ pMC.py
@@ -1,5 +1,7 @@
 import subprocess
 
+interface = "enp0"
+
 subprocess.call("ifconfig enp0 down", shell=True)
 subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
 subprocess.call("ifconfig enp0 up", shell=True)
