c7282d881672485e8a9ee490a2ac02e2cdfc3393
xTeKc <bcd3v@pm.me>
2021-06-29 23:55:06 -0400

    print interface

--- pMC.py
+++ pMC.py
@@ -2,6 +2,8 @@ import subprocess
 
 interface = "enp0"
 
-subprocess.call("ifconfig enp0 down", shell=True)
-subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig enp0 up", shell=True)
+print("[+] Changing MAC address for " + interface)
+
+# subprocess.call("ifconfig enp0 down", shell=True)
+# subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
+# subprocess.call("ifconfig enp0 up", shell=True)
