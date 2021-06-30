4d6f9a2af0fe618fd0711c1de7f5cd28b1739e40
xTeKc <bcd3v@pm.me>
2021-06-29 23:57:09 -0400

    add new_mac to print

--- pMC.py
+++ pMC.py
@@ -3,7 +3,7 @@ import subprocess
 interface = "enp0"
 new_mac = "00:11:22:33:44:66"
 
-print("[+] Changing MAC address for " + interface)
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 # subprocess.call("ifconfig enp0 down", shell=True)
 # subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
