4bdc449e1685c91408da67f44db237cda202ed07
xTeKc <bcd3v@pm.me>
2021-09-02 22:35:25 -0400

    add new file

--- pMC.py
+++ pMC.py
@@ -5,6 +5,6 @@ new_mac = "00:11:22:33:44:66"
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-# subprocess.call("ifconfig enp0 down", shell=True)
-# subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
-# subprocess.call("ifconfig enp0 up", shell=True)
+subprocess.call("ifconfig enp0 down", shell=True)
+subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig enp0 up", shell=True)
