b2174f1a9e3e244f4d86125eb7da3584d8e6d86f
xTeKc <bcd3v@pm.me>
2021-09-02 22:40:48 -0400

    change to raw input

--- inpMAC.py
+++ inpMAC.py
@@ -1,10 +1,10 @@
 import subprocess
 
-interface = input("Interface >")
-new_mac = input("New MAC > ")
+interface = raw_input("Interface > ")
+new_mac = raw_input("New MAC > ")
 
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig enp0 down", shell=True)
-subprocess.call("ifconfig enp0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig enp0 up", shell=True)
+subprocess.call("ip a enp0 down", shell=True)
+subprocess.call("ip a enp0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ip a enp0 up", shell=True)
