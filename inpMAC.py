e3678a911f92768690412788dae4d838c7ead2d5
xTeKc <bcd3v@pm.me>
2021-09-02 22:57:42 -0400

    create change_mac func and move some content to it

--- inpMAC.py
+++ inpMAC.py
@@ -1,6 +1,13 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ip a", interface, "down"])
+    subprocess.call(["ip a", interface, "hw", "ether", new_mac])
+    subprocess.call(["ip a", interface, "up"])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
@@ -11,12 +18,4 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
 interface = options.interface
 new_mac = options.new_mac
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-# subprocess.call("ip a " + interface + " down", shell=True)
-# subprocess.call("ip a " + interface + " hw ether " + new_mac, shell=True)
-# subprocess.call("ip a " + interface + " up", shell=True)
-
-subprocess.call(["ip a", interface, "down"])
-subprocess.call(["ip a", interface, "hw", "ether", new_mac])
-subprocess.call(["ip a", interface, "up"])
+change_mac()
