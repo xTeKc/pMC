a8ab201875be459e5e2ebd9907b4fa11688a88b1
xTeKc <bcd3v@pm.me>
2021-09-02 22:46:07 -0400

    add new subprocess

--- inpMAC.py
+++ inpMAC.py
@@ -7,4 +7,6 @@ print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 subprocess.call("ip a " + interface + " down", shell=True)
 subprocess.call("ip a " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ip a " + interface " up", shell=True)
+subprocess.call("ip a " + interface + " up", shell=True)
+
+subprocess.call(["ip a", interface, "down"])
