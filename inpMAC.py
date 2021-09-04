b86134c6e0addfb8fa19fd63d10c1d137b38e859
xTeKc <bcd3v@pm.me>
2021-09-03 20:42:55 -0400

    handle errors with if

--- inpMAC.py
+++ inpMAC.py
@@ -5,7 +5,12 @@ def get_args():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-    return = parser.parse_args()
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC, use --help for more info")
+    return options
 
 def change_mac(interface, new_mac):
     print(f"[+] Changing MAC address for  {interface} to {new_mac}")
@@ -13,5 +18,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["ip a", interface, "hw", "ether", new_mac])
     subprocess.call(["ip a", interface, "up"])
 
-(options, arguments) = get_args()
+ = get_args()
 change_mac(options.interface, options.new_mac)
