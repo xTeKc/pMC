f1f554850f8be11b608e029dca9507ecacba8c3d
xTeKc <bcd3v@pm.me>
2021-09-03 20:34:18 -0400

    clean up funcs

--- inpMAC.py
+++ inpMAC.py
@@ -3,15 +3,12 @@ import optparse
 
 def get_args():
     parser = optparse.OptionParser()
-
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC Address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC Address")
-
     (options, arguments) = parser.parse_args()
 
 def change_mac(interface, new_mac):
     print(f"[+] Changing MAC address for  {interface} to {new_mac}")
-
     subprocess.call(["ip a", interface, "down"])
     subprocess.call(["ip a", interface, "hw", "ether", new_mac])
     subprocess.call(["ip a", interface, "up"])
