ec0dad668a40394b1580a5a12e2edc8a2b1544a7
xTeKc <bcd3v@pm.me>
2021-09-04 22:25:50 -0400

    un comment change mac func call

--- inpMAC.py
+++ inpMAC.py
@@ -30,4 +30,4 @@ def get_current_mac(interface):
 options = get_args()
 current_mac = get_current_mac(options.interface)
 print(f"Current MAC = {str(current_mac)}")
-#change_mac(options.interface, options.new_mac)
+change_mac(options.interface, options.new_mac)
